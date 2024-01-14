e639b75961de73e2afa73f5f4538b1fc13ba36d5
BrianDYKim <18k7102dy@gmail.com>
2024-01-14 14:55:17 +0900

    command 객체 검증을 위해 field 범위로 어노테이션 설정

--- application/membership-service/src/main/kotlin/team/me/membership/application/port/in/RegisterMembershipCommand.kt
+++ application/membership-service/src/main/kotlin/team/me/membership/application/port/in/RegisterMembershipCommand.kt
@@ -3,8 +3,6 @@ package team.me.membership.application.port.`in`
 import jakarta.validation.constraints.AssertTrue
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import team.me.common.validate.SelfValidating
 
 /**
@@ -12,14 +10,15 @@ import team.me.common.validate.SelfValidating
  * @since 2023/12/03
  */
 data class RegisterMembershipCommand(
-    @NotNull
+    @field:NotNull
     val name: String,
-    @NotNull
+    @field:NotNull
     val email: String,
-    @NotNull @NotBlank
+    @field:NotNull @field:NotBlank
     val address: String,
-    @AssertTrue
+    @field:AssertTrue
     val isValid: Boolean,
+    @field:AssertTrue
     val isCorp: Boolean,
 ) : SelfValidating<RegisterMembershipCommand>() {
     init {
